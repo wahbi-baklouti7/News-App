52f600008db35a4b6f3d3f1d7d9f548b4edbd3e5
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-05-23 12:50:33 +0100

    :sparkles: add webview screen

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,51 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/screens/webview_screen.dart';
 
-Widget buildArticleItem(article,context) {
+
+Widget buildArticleItem(article, context) {
   String date = article["publishedAt"];
   date = date.substring(0, 10) + "  " + date.substring(12, 16);
 
-  return Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: Row(
-      children: [
-        Container(
-          height: 120,
-          width: 120,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                  image: NetworkImage("${article["urlToImage"]}"),
-                  fit: BoxFit.cover)),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child: Container(
+  return InkWell(
+    
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WebviewScreen(url: article["url"],)),
+      );
+    },
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
             height: 120,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Text("${article["title"]}",
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style:Theme.of(context).textTheme.subtitle1),
-                ),
-                Text("$date",
-                    style: TextStyle(fontSize: 12, color: Colors.grey))
-              ],
-            ),
+            width: 120,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    image: NetworkImage("${article["urlToImage"]}"),
+                    fit: BoxFit.cover)),
           ),
-        )
-      ],
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Container(
+              height: 120,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: Text("${article["title"]}",
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.subtitle1),
+                  ),
+                  Text("$date",
+                      style: TextStyle(fontSize: 12, color: Colors.grey))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     ),
   );
 }
 
-
-Widget articleBuilder(List list,context) {
+Widget articleBuilder(List list, context) {
   return ListView.separated(
     physics: BouncingScrollPhysics(),
     separatorBuilder: (context, index) => Divider(
@@ -54,7 +64,7 @@ Widget articleBuilder(List list,context) {
     ),
     itemCount: list.length,
     itemBuilder: (BuildContext context, index) {
-      return buildArticleItem(list[index],context);
+      return buildArticleItem(list[index], context);
     },
   );
 }
